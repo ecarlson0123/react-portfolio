2bf27438c41e3eba6282a787d79006741ac45f7f
MrVoze <ecarlson0123@yahoo.com>
2022-08-10 22:21:34 -0500

    added final project

--- src/components/PortfolioElmt/index.js
+++ src/components/PortfolioElmt/index.js
@@ -4,6 +4,7 @@ import project2 from '../../assets/personal/project2.png';
 import budgetTracker from '../../assets/personal/budget-tracker.png';
 import noteTaker from '../../assets/personal/note-taker.png'; 
 import codeQuiz from '../../assets/personal/code-quiz.png'; 
+import project3 from '../../assets/personal/project3.png';
 function Element () {
 
     const elementList = [
@@ -11,7 +12,8 @@ function Element () {
         {name: 'Watch This!', link: 'https://gentle-headland-06018.herokuapp.com/', repo:'https://github.com/bagl0025/watch-this', img: project2},
         {name: 'Budget Tracker', link: 'https://glacial-lake-04140.herokuapp.com/', repo: 'https://github.com/ecarlson0123/budget-tracker', img: budgetTracker },
         {name: 'Note Taker', link: 'https://shrouded-meadow-15877.herokuapp.com/', repo: 'https://github.com/ecarlson0123/note-taker', img: noteTaker },
-        {name: 'Code Quiz', link: 'https://ecarlson0123.github.io/code-quiz/', repo: 'https://github.com/ecarlson0123/code-quiz', img: codeQuiz }
+        {name: 'Code Quiz', link: 'https://ecarlson0123.github.io/code-quiz/', repo: 'https://github.com/ecarlson0123/code-quiz', img: codeQuiz },
+        {name: 'The Bazaar', link: 'https://shielded-earth-23841.herokuapp.com/', repo: 'https://github.com/bagl0025/The-Bazaar', img: project3 }
     ]
 
     
